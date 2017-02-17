7248694b6539bc9527de085be46cbd06341b10fd
Tobias Walle <tobias.walle@gmail.com>
2017-02-17 14:23:56 +0100

    Add Karma for testing

---
+++ karma.conf.js
@@ -0,0 +1,35 @@
+let webpackConfig = require('./webpack/webpack.client.config.development');
+
+module.exports = (config) => {
+  config.set({
+    plugins: [
+      require('karma-webpack'),
+      require('karma-tap'),
+      require('karma-chrome-launcher'),
+    ],
+
+    basePath: '',
+    frameworks: ['tap'],
+    files: ['**/*.spec.ts*'],
+
+    preprocessors: {
+      '**/*.ts*': ['webpack'],
+    },
+
+    webpack: {
+      resolve: webpackConfig.resolve,
+      module: webpackConfig.module,
+      node: {
+        fs: 'empty'
+      }
+    },
+
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome', 'Chromium'],
+    singleRun: false
+  })
+};
