9bd24eced44840a943a63c4ecf3c3c295c6620fd
Tobias Walle <tobias.walle@gmail.com>
2017-02-17 12:43:28 +0100

    Add 404 page

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Route, IndexRedirect } from 'react-router';
 import { BlogRoutes } from "./modules/blog/blog.routes";
+import { NotFoundComponent } from "./components/not-found.component";
 
 export const routes: JSX.Element = (
   <Route path="/">
     <IndexRedirect to="/blog"/>
     {BlogRoutes()}
+    <Route path="*" component={NotFoundComponent}/>
   </Route>
 );
