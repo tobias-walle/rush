84ed6757258b742133f1a0bc9f23aaf7d1048cf4
Tobias Walle <tobias.walle@gmail.com>
2017-02-19 21:19:21 +0100

    Add tests for Article list item component

---
+++ src/app/modules/blog/components/article-list-item.component.spec.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { shallow, mount } from "enzyme";
+import { Article } from "../models/article";
+import { ArticleListItemComponent } from "./article-list-item.component";
+
+describe('ArticleListItemComponent', () => {
+  it('should render', () => {
+    expect(shallow(<ArticleListItemComponent/>).exists()).toBeTruthy();
+  });
+
+  it('should render Article', () => {
+    let article = new Article('Subject 1', 'Body 1');
+
+    let wrapper = shallow(<ArticleListItemComponent article={article}/>);
+
+    let html = wrapper.html();
+    expect(html.includes(article.subject)).toBeTruthy('Does not render subject');
+    expect(html.includes(article.body)).toBeTruthy('Does not render body');
+  });
+
+  it('should trigger delete callback', () => {
+    let article = new Article('Subject 1', 'Body 1');
+    let deleteArticle = jasmine.createSpy('deleteArticle');
+
+    let wrapper = shallow(
+      <ArticleListItemComponent
+        article={article}
+        onDeleteClicked={deleteArticle}
+      />
+    );
+
+    wrapper.find('.button-delete').first().simulate('click');
+    expect(deleteArticle).toHaveBeenCalled();
+  });
+});
