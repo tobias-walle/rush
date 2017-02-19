ff4cf0712a768013310a5fd384464efdb4cfa11b
Tobias Walle <tobias.walle@gmail.com>
2017-02-19 21:22:16 +0100

    Remove unused import

--- src/app/modules/blog/components/article-list-item.component.spec.tsx
+++ src/app/modules/blog/components/article-list-item.component.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import { Article } from "../models/article";
 import { ArticleListItemComponent } from "./article-list-item.component";
 
