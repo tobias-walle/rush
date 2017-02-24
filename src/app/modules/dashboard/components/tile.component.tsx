4b6eddae2f218edc915b5b923e7d4ce10890ebae
Tobias Walle <tobias.walle@maibornwolff.de>
2017-02-24 16:27:12 +0000

    Add tile component

---
+++ src/app/modules/dashboard/components/tile.component.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { WithStyles } from 'isomorphic-style-loader-utils';
+const styles = require('./tile.component.scss');
+
+export class TileProps {
+  component?: React.Component<any, any>;
+  height?: number = 1;
+  width?: number = 1;
+}
+
+@WithStyles(styles)
+export class TileComponent extends React.Component<TileProps, any> {
+  render() {
+    return (
+      <div className={styles.tile}
+      >Tile</div>
+    );
+  }
+}
