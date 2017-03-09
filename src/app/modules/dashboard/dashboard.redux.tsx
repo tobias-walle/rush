b116c995bf8ba062e868e3be443539e47f75feb5
Tobias Walle <tobias.walle@maibornwolff.de>
2017-03-09 21:45:05 +0000

    Change appearance of dashboard

--- src/app/modules/dashboard/dashboard.redux.tsx
+++ src/app/modules/dashboard/dashboard.redux.tsx
@@ -9,6 +9,12 @@ const initialState: DashboardState = {
     new TileSelection('1', { 'text': '1' }),
     new TileSelection('2', { 'text': '2' }),
     new TileSelection('2', { 'text': '3' }),
+    new TileSelection('1', { 'text': '4' }),
+    new TileSelection('2', { 'text': '5' }),
+    new TileSelection('2', { 'text': '6' }),
+    new TileSelection('2', { 'text': '7' }),
+    new TileSelection('1', { 'text': '8' }),
+    new TileSelection('1', { 'text': '9' }),
   ],
 };
 
