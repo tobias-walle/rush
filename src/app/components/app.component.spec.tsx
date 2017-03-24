76b925de45717c64a96435805dc4b6f93363c30d
Tobias Walle <tobias.walle@gmail.com>
2017-03-24 23:54:46 +0100

    Fix Test

--- src/app/components/app.component.spec.tsx
+++ src/app/components/app.component.spec.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
-
 import { AppComponent } from './app.component';
 import { createMemoryHistory } from 'react-router';
 import { Store } from 'react-redux';
 import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
-
 const mockStore = configureStore();
 
 describe('AppComponent', () => {
   it('should render', () => {
-    const history = createMemoryHistory(['/'] as any);
+    const history = createMemoryHistory();
     const store: Store<any> = mockStore() as any;
 
     expect(shallow(
