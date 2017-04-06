a80cb34a52b4308f5207a792698f3a3eb7034bbe
Tobias Walle <tobias.walle@gmail.com>
2017-04-06 20:36:07 +0200

    Add missing react-router-redux and refactoring

--- src/app/modules/root.ts
+++ src/app/modules/root.ts
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import { routerReducer } from 'react-router-redux';
 import { combineEpics } from 'redux-observable';
-import { AppComponent } from './app/app/app.component';
+import { AppComponent } from './app/app.component';
 
 // Combine all modules to the global state interface
 export interface GlobalState {
