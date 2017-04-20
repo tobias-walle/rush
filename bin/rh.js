915a7d73ab0e5419d1eaf12e4a4e96df7f155097
Tobias Walle <tobias.walle@maibornwolff.de>
2017-04-20 16:05:15 +0100

    Fix rh command

--- bin/rh.js
+++ bin/rh.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const spawn = require('child_process').spawn;
-const yoCommand = 'yo';
+const isWindows = /^win/.test(process.platform);
+const yoCommand = isWindows ? 'yo.cmd' : 'yo';
 const args = process.argv;
 const firstArgument = args[2] || '';
 const isFirstArgumentAnOption = firstArgument[0] === '-';
@@ -8,5 +9,8 @@ const subGenerator = firstArgument && !isFirstArgumentAnOption ? firstArgument :
 const generatorName = `rush:${subGenerator}`;
 const parameterStart = isFirstArgumentAnOption ? 2 : 3;
 
-spawn(yoCommand, [generatorName].concat(args.slice(parameterStart)), {stdio: 'inherit'});
+spawn(yoCommand, [generatorName].concat(args.slice(parameterStart)), {stdio: 'inherit'})
+  .on('error', err => {
+    throw err;
+  });
 
