d8f5608487db9c8c10488d528994699f100afe23
Tobias Walle <tobias.walle@gmail.com>
2017-07-15 20:52:12 +0200

    Use generator options decorator in module generator

--- generators/module/index.js
+++ generators/module/index.js
@@ -1,42 +1,19 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const validate = require('../../utils/validate-utils');
 
-module.exports = class extends Generator {
-
-  constructor(args, opts) {
-    super(args, opts);
-
-    // Setup object for custom settings
-    this.settings = {};
-
-    this.argument('name', {
-      type: String,
-      required: true,
-      desc: 'The name of the module'
-    });
-  }
-
+const ModuleGenerator = class extends Generator {
   initializing() {
     const name = this.options.name;
 
-    // Check if name is valid
-    if (name === undefined) {
-      this.env.error('The component name has to be defined');
-    }
-    if (!validate.isLispCase(name)) {
-      this.env.error('Only lower case characters and hyphens are allowed in the component name');
-    }
-
     // Setup destination
-    this.settings.destination = this.destinationPath('src', 'app', 'modules', name);
+    const destination = this.destinationPath('src', 'app', 'modules', name);
 
     // Setup other Generators
     this.composeWith(require.resolve('../component'),
       {
         arguments: [name],
         noBasePath: true,
-        destination: this.settings.destination,
+        destination: destination,
         flat: true
       }
     );
@@ -44,11 +21,13 @@ module.exports = class extends Generator {
       {
         arguments: [name],
         moduleName: name,
-        destination: this.settings.destination
+        destination: destination
       }
     );
   }
-
-  writing() {
-  }
 };
+
+const decorateGeneratorOptions = require('../../decorator/generator-options.decorator');
+const options = require('../../configuration/module-options');
+
+module.exports = decorateGeneratorOptions(ModuleGenerator, 'module', options);
